3ce6e8e88a5d6482b55bbf06f8dddd5802dd4f72
fulin426 <liufam@ms-MacBook-Pro.local>
2019-03-18 23:22:25 -0700

    heroku build

---
+++ client/build/precache-manifest.79385475ebf5401fab541fc9a9c7c27e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6a6375cdcf6d4c2b8514",
+    "url": "/static/js/main.394f5690.chunk.js"
+  },
+  {
+    "revision": "eb1defbaac4beb2ce54c",
+    "url": "/static/js/2.c9394aa8.chunk.js"
+  },
+  {
+    "revision": "6a6375cdcf6d4c2b8514",
+    "url": "/static/css/main.7f448f74.chunk.css"
+  },
+  {
+    "revision": "336cfa9cee2e85e3e3ea5da21b018552",
+    "url": "/index.html"
+  }
+];
