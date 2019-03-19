bb0e06d7a1d74c82efe6771b988cfa8713120d96
fulin426 <liufam@ms-MacBook-Pro.local>
2019-03-18 23:08:28 -0700

    added server with backend call to colormind api

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 const Clarifai = require('clarifai');
+const app = new Clarifai.App({apiKey: 'bd8644854b19417dacdfa3adba21aab1'});
 //Convert Hex to RGB functions
 function hexToR(h) {return parseInt((cutHex(h)).substring(0,2),16)};
 function hexToG(h) {return parseInt((cutHex(h)).substring(2,4),16)};
@@ -15,47 +16,24 @@ function toHex(n) {
       + "0123456789ABCDEF".charAt(n%16);
 };
 
-export const analyzeImage = (url) => async dispatch => {
-  const app = new Clarifai.App({apiKey: 'bd8644854b19417dacdfa3adba21aab1'});
-    app.models.predict(Clarifai.COLOR_MODEL, url)
-    .then(response => {
-      //convert Hexcolors from Clarifai API to send to Colormind API
+export const analyzeImage = (url) => dispatch => {
+  app.models.predict("eeed0b6733a644cea07cf4c60f87ebb7", url)
+  .then(function(response) {
       const hexColors = [];
       response.outputs[0].data.colors.map(color => {
-        return hexColors.push(color.raw_hex);
+        return hexColors.push(color.raw_hex.slice(1, color.raw_hex.length));
       });
-
-      let hexToRGB = [];
-      for (let i = 0; i < hexColors.length; i++) {
-        let R = hexToR(hexColors[i]);
-        let G = hexToG(hexColors[i]);
-        let B = hexToB(hexColors[i]);
-        hexToRGB.push([ R, G , B ]);
-      };
-
-      if( hexToRGB.length > 5 ) {
-        hexToRGB = hexToRGB.slice(1, 6);
-      };
-
-      if (hexToRGB.length < 5) {
-        for (let i = hexToRGB.length; i < 5 ; i++) {
-          hexToRGB.push('N');
-        }
-      }
-      //2nd call to colormind API
-      axios.post('http://colormind.io/api/', {
-        model : "default",
-        input : hexToRGB
-      })
-      .then(function (response) {
-        let RGBtoHexData = [];
-        let colorMindResponse = response.data.result;
-        for (let i = 0; i < colorMindResponse.length; i++) {
-          RGBtoHexData.push({
-            hexColor: rgbToHex(colorMindResponse[i][0], colorMindResponse[i][1], colorMindResponse[i][2]),
-            alpha: 1
-          });
-        }
+      fetch('/api/colormind?colors=' + hexColors)
+        .then(res => res.json())
+        .then(function (response) {
+          let RGBtoHexData = [];
+          let colorMindResponse = response;
+          for (let i = 0; i < colorMindResponse.length; i++) {
+            RGBtoHexData.push({
+              hexColor: rgbToHex(colorMindResponse[i][0], colorMindResponse[i][1], colorMindResponse[i][2]),
+              alpha: 1
+            });
+          }
         // console.log(RGBtoHexData);
         dispatch({
           type: 'ANALYZE_IMAGE',
@@ -72,26 +50,6 @@ export const analyzeImage = (url) => async dispatch => {
   })
 };
 
-//finish error handling in reducer
-export const randomImage = () => async dispatch => {
-  try {
-    const response = await axios.get('https://api.unsplash.com/photos/random', {
-      headers: {
-        Authorization: 'Client-ID 7e3664d943831b0902bdde147b893f182272b23dad6fcf084fb3eaa9d4b0b325'
-      }
-    });
-    dispatch({
-      type: 'RANDOM_IMAGE',
-      url: response.data.urls.regular
-    })
-  } catch(err) {
-    console.log(err);
-    dispatch({
-      type: 'RANDOM_IMAGE_ERROR',
-      error: err
-    })
-  }
-};
 //add error handling
 export const sendColorInfo = (hexColor) => dispatch => {
 
