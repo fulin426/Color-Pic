8811741cbc6200e012d0e3146be3bb8481452d84
fulin426 <liufam@ms-MacBook-Pro.local>
2019-03-31 23:22:45 -0700

    updated server.js for heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 const ColorPalette = require('./routes/colorPalette');
 const colormindAPI = require('./routes/colormindAPI');
@@ -19,9 +20,14 @@ mongoose.connect(db, { useNewUrlParser: true })
 // uses Heroku Env or Port 5000
 app.set("port", process.env.PORT || 5000);
 
-// Express only serves static assets in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// Express only serves static assets in productio
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // Use Route for colormind API
