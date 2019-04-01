830ca2a7f662931479741bbddfbfa3d7b3ae7279
fulin426 <liufam@ms-MacBook-Pro.local>
2019-03-31 23:46:01 -0700

    manual build

--- server.js
+++ server.js
@@ -30,10 +30,10 @@ app.use('/api/colors', ColorPalette);
 if (process.env.NODE_ENV === 'production') {
   //set static folder
   app.use(express.static('client/build'));
+
   // app.get('*', (req, res) => {
   //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   // });
-  // "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
 }
 
 app.listen(app.get("port"), () => {
