352f508a6f04ecb562366f44c1a0bb8907e6aab3
fulin426 <liufam@ms-MacBook-Pro.local>
2019-04-02 22:12:33 -0700

    server.js wildcard

--- server.js
+++ server.js
@@ -32,6 +32,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
 app.listen(app.get("port"), () => {
   console.log(`Find the server at: http://localhost:${app.get("port")}/`);
 });
