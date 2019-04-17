ceb378cdc2b59e7cfae699a1fc8e60c169a3c3fc
fulin426 <liufam@ms-MacBook-Pro.local>
2019-04-16 23:23:01 -0700

    links and icons added to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Icon, Container } from 'semantic-ui-react';
 
 const Footer = ({url}) => {
   return(
-    <div className="footer">
-      © 2019
-    </div>
+    <Container className="footer">
+      <h5 className="footer-header">
+        © 2019
+      </h5>
+      <div className="icon-container">
+        <a className="icon-div" href="https://github.com/fulin426/Color-Pic" target="_blank">
+          <Icon name="github" size='big' />
+        </a>
+        <a className="icon-div" href="https://www.linkedin.com/in/fu-lin-liu/" target="_blank">
+          <Icon className="icon-div" name="linkedin square" size='big' />
+        </a>
+        <a className="icon-div" href="mailto:fulin426@gmail.com" target="_blank">
+          <Icon className="icon-div" name="mail" size='big' />
+        </a>
+      </div>
+    </Container>
   );
 };
 
