4ecc503f357e902fe2e00d8add99604bcf032d53
fulin426 <liufam@ms-MacBook-Pro.local>
2019-04-27 07:12:42 -0700

    formated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,35 +4,37 @@ import "./css/footer.css";
 
 const Footer = ({ url }) => {
   return (
-    <Container className="footer">
-      <h5 className="footer-header">© 2019</h5>
-      <div className="icon-container">
-        <a
-          className="icon-div"
-          href="https://github.com/fulin426/Color-Pic"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon className="icon-div" name="github" size="big" />
-        </a>
-        <a
-          className="icon-div"
-          href="https://www.linkedin.com/in/fu-lin-liu/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon className="icon-div" name="linkedin square" size="big" />
-        </a>
-        <a
-          className="icon-div"
-          href="mailto:fulin426@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Icon className="icon-div" name="mail" size="big" />
-        </a>
-      </div>
-    </Container>
+    <div>
+      <Container className="footer">
+        <h5 className="footer-header">© 2019</h5>
+        <div className="icon-container">
+          <a
+            className="icon-div"
+            href="https://github.com/fulin426/Color-Pic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon className="icon-div" name="github" size="big" />
+          </a>
+          <a
+            className="icon-div"
+            href="https://www.linkedin.com/in/fu-lin-liu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon className="icon-div" name="linkedin square" size="big" />
+          </a>
+          <a
+            className="icon-div"
+            href="mailto:fulin426@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon className="icon-div" name="mail" size="big" />
+          </a>
+        </div>
+      </Container>
+    </div>
   );
 };
 
