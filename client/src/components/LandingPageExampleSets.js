6a814d4ae06cbc6110d3603454207a1504a65e02
fulin426 <liufam@ms-MacBook-Pro.local>
2019-04-28 21:25:30 -0700

    made example images more compact

--- client/src/components/LandingPageExampleSets.js
+++ client/src/components/LandingPageExampleSets.js
@@ -19,27 +19,53 @@ const LandingPageExampleSets = () => {
     { FujiExample: ["#ECE3E0", "#252C32", "#6A707E", "#53262E", "#B81F34"] },
     { DabinExample: ["#684C56", "#EDDFD0", "#87AC95", "#AA8489", "#884B86"] },
     { CameraExample: ["#26221A", "#3D464F", "#96BBDC", "#74AACB", "#867D80"] },
-    { BladeRunnerExample: [ "#804C27", "#F7AC49", "#CD6622", "#7A2717", "#2C2022"]}
+    {
+      BladeRunnerExample: [
+        "#804C27",
+        "#F7AC49",
+        "#CD6622",
+        "#7A2717",
+        "#2C2022"
+      ]
+    }
   ];
 
   return (
     <Grid>
       <Grid.Row>
-        <Grid.Column width={8}>
+        <Grid.Column
+          className="column-overide-left"
+          computer={4}
+          tablet={4}
+          mobile={8}
+        >
           <Image src={FujiExample} />
           {renderOneColorSet(exampleSets[0].FujiExample)}
         </Grid.Column>
-        <Grid.Column width={8}>
+        <Grid.Column
+          className="column-overide-right"
+          computer={4}
+          tablet={4}
+          mobile={8}
+        >
           <Image src={DabinExample} />
           {renderOneColorSet(exampleSets[1].DabinExample)}
         </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column width={8}>
+        <Grid.Column
+          className="column-overide-left"
+          computer={4}
+          tablet={4}
+          mobile={8}
+        >
           <Image src={CameraExample} />
           {renderOneColorSet(exampleSets[2].CameraExample)}
         </Grid.Column>
-        <Grid.Column width={8}>
+        <Grid.Column
+          className="column-overide-right"
+          computer={4}
+          tablet={4}
+          mobile={8}
+        >
           <Image src={BladeRunnerExample} />
           {renderOneColorSet(exampleSets[3].BladeRunnerExample)}
         </Grid.Column>
